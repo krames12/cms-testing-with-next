64d6dd1be2601e89a9829dedcaa09d8b50e8eb8a
Nick Kramer <nkramer@fanatics.com>
2023-06-19 13:47:43 -0400

    adds working image slider w/ thumbnails

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,8 +1,10 @@
+import {useState} from 'react'
 import { useRouter } from 'next/router'
 import ErrorPage from 'next/error'
 import Head from 'next/head'
 import Image from 'next/image';
-import Swiper from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Thumbs } from 'swiper';
 import 'swiper/css';
 
 import Container from '../../components/container'
@@ -15,38 +17,13 @@ import { getProductByProductId } from '../../lib/productApi'
 
 export default function Product({product, preview}) {
   const router = useRouter()
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
 
   const productImages = product?.productImagesCollection?.items ?? [];
 
   if (!router.isFallback && !product) {
     return <ErrorPage statusCode={404} />
   }
-
-  const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    keyboard: {
-      enabled: true,
-    },
-    centeredSlides: true,
-    createElements: true,
-    // If we need pagination
-    // pagination: {
-    //   el: '.swiper-pagination',
-    // },
-  
-    // Navigation arrows
-    // navigation: {
-    //   nextEl: '.swiper-button-next',
-    //   prevEl: '.swiper-button-prev',
-    // },
-  
-    // And if we need scrollbar
-    // scrollbar: {
-    //   el: '.swiper-scrollbar',
-    // },
-  });
 
   return (
     <Layout preview={preview}>
@@ -65,17 +42,48 @@ export default function Product({product, preview}) {
               </Head>
               <div className="grid-cols-1 lg:grid-cols-2">
                 {productImages ? (
-                  <div className="swiper ">
+                  <>
+                    <Swiper
+                      modules={[Thumbs]}
+                      thumbs={{ swiper: thumbsSwiper }}
+                      spaceBetween={50}
+                      slidesPerView={1}
+                      onSlideChange={() => console.log('slide change')}
+                      onSwiper={(swiper) => console.log(swiper)}
+                    >
+                      {productImages.map( ({description, fileName, url, height, width}) => (
+                        <SwiperSlide>
+                          <Image 
+                            loader={() => url}
+                            src={fileName}
+                            width={width}
+                            height={height}
+                            alt={description}
+                          />
+                        </SwiperSlide>
+                      ))}
+                    </Swiper>
+                    
+                    {/* Thumbnails */}
+                    <Swiper
+                    modules={[Thumbs]}
+                    watchSlidesProgress
+                    onSwiper={setThumbsSwiper}
+                    slidesPerView={6}
+                  >
                     {productImages.map( ({description, fileName, url, height, width}) => (
-                      <Image 
-                        loader={() => url}
-                        src={fileName}
-                        width={width}
-                        height={height}
-                        alt={description}
-                      />
+                      <SwiperSlide>
+                        <Image 
+                          loader={() => url}
+                          src={fileName}
+                          width={width}
+                          height={height}
+                          alt={description}
+                        />
+                      </SwiperSlide>
                     ))}
-                  </div>
+                  </Swiper>
+                </>
                 ) : null}
                 <h1 className="text-2xl">{product.name}</h1>
               </div>
