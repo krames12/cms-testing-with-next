15d2a827af504c6502c16e63fe6d19513e3c31fb
Nick Kramer <nkramer@fanatics.com>
2023-06-21 14:26:18 -0400

    adds storybook and storybook config

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,13 @@
+import { storyblokInit, apiPlugin } from "@storyblok/react";
 import '../styles/index.css'
+ 
+storyblokInit({
+  accessToken: process.env.STORYBLOK_PREVIEW_TOKEN,
+  use: [apiPlugin],
+  apiOptions: {
+    region: "us",
+  },
+});
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
